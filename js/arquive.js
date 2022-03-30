0a91017f916508246b2f1819e2f818060bf0023f
caiovictorsoares <caiovictorfamilia@gmail.com>
2022-03-30 15:31:15 -0300

    calcsons_30032022
    calcsons_30032022_15:30
    Fiz uma função para a raiz quadrada e vou criar outra para potencia.

--- js/arquive.js
+++ js/arquive.js
@@ -1,7 +1,12 @@
+//#region Variáveis Globais
+var Resultado = document.getElementById('Resultado').innerHTML
+//#endregion
+
+//#region metodos Básicos
  function insert(num)
 {
     var numero = document.getElementById('Resultado').innerHTML 
-    document.getElementById('Resultado').innerHTML= numero+num
+    document.getElementById('Resultado').innerHTML = numero+num
 }
 function clean()
 {
@@ -9,14 +14,27 @@ function clean()
 }
 function back()
 {
-    var Resultado = document.getElementById('Resultado').innerHTML
     document.getElementById('Resultado').innerHTML = Resultado.substring(0, Resultado.length -1)
-} 
+}
+//#endregion 
+
+//#region metodos cálculos
 function calcular()
 {
-    var Resultado = document.getElementById('Resultado').innerHTML
     if(Resultado)
     {
         document.getElementById('Resultado').innerHTML = eval(Resultado)
     }
 }
+
+function calcularRaiz()
+{
+    var calculo = parseInt(Resultado);
+    var raiz = (Math.sqrt(calculo));
+    document.getElementById('Resultado').innerHTML = raiz
+}
+function calcularPotencia()
+{
+    var Resulta
+}
+//#endregion
